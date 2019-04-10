c503c2996992de29a59c310066fe5aa5ce552b92
chao <liangchaotech@gmail.com>
2019-04-10 19:10:38 +0930

    created userStory1 and 2 created code before for userStory1 and 2

---
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint1/task1/Task1App.java
@@ -0,0 +1,19 @@
+package edu.cdu.xeon.sprint1.task1;
+
+import edu.cdu.xeon.sprint1.task1.before.Animal;
+import edu.cdu.xeon.sprint1.task1.before.Bird;
+import edu.cdu.xeon.sprint1.task1.before.Dog;
+
+public class Task1App {
+
+    public static void main(String[] args){
+
+        Animal sparky = new Dog();
+        Animal tweety = new Bird();
+
+        sparky.fly();
+        tweety.fly();
+
+
+    }
+}
--- scrumpractices/src/main/java/edu/cdu/xeon/sprint1/task1/before/App.java
+++
@@ -1,4 +0,0 @@
-package edu.cdu.xeon.sprint1.task1.before;
-
-public class App {
-}
