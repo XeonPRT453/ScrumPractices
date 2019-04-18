bcaf56bb235cf6b1ecea189ba63f3adcacd69aa8
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2019-04-19 02:33:07 +0930

    Update App.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint1/s299806/task2/after/App.java
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint1/s299806/task2/after/App.java
@@ -1,4 +1,59 @@
 package edu.cdu.xeon.sprint1.s299806.task2.after;
 
-public class App {
+
+class BankAccount {
+	private int accountAge;
+	private int creditScore;
+	private AccountInterest accountInterest;
+	
+	public BankAccount(int accountAge, int creditScore, AccountInterest accountInterest) {
+		this.accountAge = accountAge;
+		this.creditScore = creditScore;
+		this.accountInterest = accountInterest;
+	}
+	
+	public int getAccountAge() {
+		return this.accountAge;
+	}
+	
+	public int getCreditScore() {
+		return this.creditScore;
+	}
+	
+	public AccountInterest getAccountInterest() {
+		return this.accountInterest;
+	}
+}
+ 
+class AccountInterest {
+	private BankAccount account;
+	
+	public AccountInterest(BankAccount account) {
+		this.account = account;
+	}
+	
+	public BankAccount getAccount() {
+		return this.account;
+	}
+	
+	public double getInterestRate() {
+		return calculateInterestRate();
+	}
+	
+	public boolean isIntroductoryRate() {
+		return (calculateInterestRate() < 0.05);
+	}
+	
+	//将calculateInterestRate()方法从BankAccount类搬移到AccountInterest类
+    public double calculateInterestRate() {
+		if (account.getCreditScore() > 800) {
+			return 0.02;
+		}
+		
+		if (account.getAccountAge() > 10) {
+			return 0.03;
+		}
+		
+		return 0.05;
+	}
 }
