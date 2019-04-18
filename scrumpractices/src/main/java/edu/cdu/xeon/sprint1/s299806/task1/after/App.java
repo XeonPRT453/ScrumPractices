21f6c5905ea27e6818287b1eba501787705720fb
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2019-04-19 02:12:22 +0930

    Update App.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint1/s299806/task1/after/App.java
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint1/s299806/task1/after/App.java
@@ -1,4 +1,29 @@
 package edu.cdu.xeon.sprint1.s299806.task1.after;
 
-public class App {
+public class QuizFactory {
+  
+public Quiz makeQuiz(QuizProperty property, int count) throws IllegalArgumentException{ if (count > property.getAllProblems().size())
+throw new IllegalArgumentException("The number of problems in quiz must be less than "+property.getAllProblems().size());
+Random rnd = new Random(); Vector quizChosen = new Vector(); Hashtable quizLooked = new Hashtable(); BasicProblemParser parser = new BasicProblemParser(); try{
+
+while (quizChosen.size() < count){
+
+int i = rnd.nextInt(property.getAllProblems().size()); String prob = (String)property.getAllProblems().get(i); if (!quizLooked.containsKey(prob)){
+
+quizLooked.put(prob,prob);
+parser.parse(prob);
+quizChosen.add( new Problem( prob,parser.calculate()));
 }
+}
+}catch(IllegalArgumentException iae){
+throw new IllegalArgumentException("The quiz type "+property.getType "'s problem is in an illegal format. & "+iae);
+}
+
+return new Quiz(property.getType(), quizChosen);
+
+}
+
+}
+
+}
+//QuizFactory class after Move Method applied
