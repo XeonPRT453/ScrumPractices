c2f3bd4a38fc81d0bc552ddf41fda56ae5fe644b
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2019-04-19 02:59:30 +0930

    Update App.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint2/s299806/task1/before/App.java
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint2/s299806/task1/before/App.java
@@ -1,4 +1,25 @@
 package edu.cdu.xeon.sprint2.s299806.task1.before;
 
-public class App {
-}
+public class ClassRegistration
+    {
+        public void Create()
+        {
+            // create registration code
+        }
+
+        public void Transfer()
+        {
+            // class transfer code
+        }
+
+        public decimal Total { get; private set; }
+    }
+
+    public class RegistrationProcessor
+    {
+        public decimal ProcessRegistration(ClassRegistration registration)
+        {
+            registration.Create();
+            return registration.Total;
+        }
+    }
