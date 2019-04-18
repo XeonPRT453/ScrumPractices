30ccdc222d9580af92896ce288b3feff16e59430
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2019-04-19 02:47:22 +0930

    Update App.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint1/s299806/task3/before/App.java
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint1/s299806/task3/before/App.java
@@ -1,4 +1,36 @@
 package edu.cdu.xeon.sprint1.s299806.task3.before;
 
 public class App {
+   public class Order
+    {
+        private List<OrderLine> _orderLines;
+        private double _orderTotal;
+
+        public IList<OrderLine> OrderLines
+        {
+            get { return _orderLines; }
+        }
+
+        public void AddOrderLine(OrderLine orderLine)
+        {
+            _orderTotal += orderLine.Total;
+            _orderLines.Add(orderLine);
+        }
+
+        public void RemoveOrderLine(OrderLine orderLine)
+        {
+            orderLine = _orderLines.Find(o => o == orderLine);
+
+            if (orderLine == null)
+                return;
+
+            _orderTotal -= orderLine.Total;
+            _orderLines.Remove(orderLine);
+        }
+    }
+
+    public class OrderLine
+    {
+        public double Total { get; private set; }
+    }
 }
