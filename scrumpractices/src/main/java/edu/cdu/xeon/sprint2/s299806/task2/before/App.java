fc5248780788379bd900d33975defce40dfb1d35
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2019-04-19 03:09:14 +0930

    Update App.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint2/s299806/task2/before/App.java
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint2/s299806/task2/before/App.java
@@ -1,4 +1,50 @@
 package edu.cdu.xeon.sprint2.s299806.task2.before;
 
-public class App {
-}
+public class ClientCode
+    {
+        public decimal CalculateShipping()
+        {
+            ShippingInfo shippingInfo = new ShippingInfo();
+            return shippingInfo.CalculateShippingAmount(State.Alaska);
+        }
+    }
+
+    public enum State
+    {
+        Alaska,
+        NewYork,
+        Florida
+    }
+
+    public class ShippingInfo
+    {
+        public decimal CalculateShippingAmount(State shipToState)
+        {
+            switch (shipToState)
+            {
+                case State.Alaska:
+                    return GetAlaskaShippingAmount();
+                case State.NewYork:
+                    return GetNewYorkShippingAmount();
+                case State.Florida:
+                    return GetFloridaShippingAmount();
+                default:
+                    return 0m;
+            }
+        }
+
+        private decimal GetAlaskaShippingAmount()
+        {
+            return 15m;
+        }
+
+        private decimal GetNewYorkShippingAmount()
+        {
+            return 10m;
+        }
+
+        private decimal GetFloridaShippingAmount()
+        {
+            return 3m;
+        }
+    }
