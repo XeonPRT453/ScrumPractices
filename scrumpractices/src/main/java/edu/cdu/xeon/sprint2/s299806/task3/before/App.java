636e45092ae7b5a1838efd22cfe02b08ca71a12c
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2019-04-19 03:15:40 +0930

    Update App.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint2/s299806/task3/before/App.java
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint2/s299806/task3/before/App.java
@@ -1,4 +1,24 @@
 package edu.cdu.xeon.sprint2.s299806.task3.before;
 
-public class App {
-}
+ public class Video 
+   { 
+        public void PayFee(decimal fee) 
+        { 
+        } 
+
+        public void RentVideo(Video video, Customer customer) 
+        { 
+            customer.Videos.Add(video); 
+        } 
+
+        public decimal CalculateBalance(Customer customer) 
+        { 
+            returncustomer.LateFees.Sum(); 
+        } 
+    } 
+
+    public class Customer 
+   { 
+        public IList<decimal> LateFees { get; set; } 
+        public IList<Video> Videos { get; set; } 
+    } 
