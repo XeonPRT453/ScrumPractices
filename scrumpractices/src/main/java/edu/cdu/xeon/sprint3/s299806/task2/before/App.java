06eaefa56ab449f75d837f938899b7587a4fb1a9
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2019-05-03 03:24:41 +0930

    Update App.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint3/s299806/task2/before/App.java
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint3/s299806/task2/before/App.java
@@ -1,4 +1,47 @@
-package edu.cdu.xeon.sprint3.s299806.task2.before;
+using System.Collections.Generic;
+using System.Linq;
 
-public class App {
+namespace LosTechies.DaysOfRefactoring.SampleCode.BreakMethod.Before
+{
+    public class CashRegister
+    {
+        public CashRegister()
+        {
+            Tax = 0.06m;
+        }
+
+        private decimal Tax { get; set; }
+
+        public void AcceptPayment(Customer customer, IEnumerable<Product> products, decimal payment)
+        {
+            decimal subTotal = 0m;
+            foreach (Product product in products)
+            {
+                subTotal += product.Price;
+            }
+
+            foreach (Product product in products)
+            {
+                subTotal -= product.AvailableDiscounts;
+            }
+
+            decimal grandTotal = subTotal * Tax;
+
+            customer.DeductFromAccountBalance(grandTotal);
+        }
+    }
+
+    public class Customer
+    {
+        public void DeductFromAccountBalance(decimal amount)
+        {
+            // deduct from balance
+        }
+    }
+
+    public class Product
+    {
+        public decimal Price { get; set; }
+        public decimal AvailableDiscounts { get; set; }
+    }
 }
