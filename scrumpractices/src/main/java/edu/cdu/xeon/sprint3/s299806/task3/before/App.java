6ed39e983f59e6066f1a889348f5d9daf849eb96
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2019-05-03 03:29:13 +0930

    Update App.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint3/s299806/task3/before/App.java
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint3/s299806/task3/before/App.java
@@ -1,4 +1,38 @@
-package edu.cdu.xeon.sprint3.s299806.task3.before;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-public class App {
+namespace LosTechies.DaysOfRefactoring.SampleCode.ArrowheadAntipattern.Before
+{
+    public class Security
+    {
+        public ISecurityChecker SecurityChecker { get; set; }
+
+        public Security(ISecurityChecker securityChecker)
+        {
+            SecurityChecker = securityChecker;
+        }
+
+        public bool HasAccess(User user, Permission permission, IEnumerable<Permission> exemptions)
+        {
+            bool hasPermission = false;
+
+            if (user != null)
+            {
+                if (permission != null)
+                {
+                    if (exemptions.Count() == 0)
+                    {
+                        if (SecurityChecker.CheckPermission(user, permission) || exemptions.Contains(permission))
+                        {
+                            hasPermission = true;
+                        }
+                    }
+                }
+            }
+
+            return hasPermission;
+        }
+    }
 }
