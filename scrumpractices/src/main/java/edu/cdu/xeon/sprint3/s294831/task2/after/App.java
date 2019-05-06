cd64fb6c8be518daed88219786695aeea462a06f
shraddha294831 <30825232+shraddha294831@users.noreply.github.com>
2019-05-06 20:57:15 +0930

    Update and rename App.java to Private_Class_Data.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint3/s294831/task2/after/App.java
+++
@@ -1,4 +0,0 @@
-package edu.cdu.xeon.sprint3.s294831.task2.after;
-
-public class App {
-}
---
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint3/s294831/task2/after/Private_Class_Data.java
@@ -0,0 +1,41 @@
+package edu.cdu.xeon.sprint3.s294831.task2.after;
+
+public class Private_Class_Data {
+  
+public class CalculateMaths{
+private double division;
+private int add;
+private int sub;
+public CalculateMaths(double division, int add, int sub) {
+this.double = division;
+this.int = add;
+this.int = sub;
+}
+public double Maths{
+get { return this.division; }
+}
+public int add {
+get { return this.add; }
+}
+public int Sub {
+get { return this.sub; }
+}
+}
+public class Maths{
+private CalculateMaths calculateMaths;
+public Maths(double division , int add, int sub) {
+this.CalculateMaths = new calculateMaths(division , add, sub);
+}
+public int add {
+get { return this.add. 5+ 66; }
+}
+public int sub {
+get { return this.sub. 20-3; }
+public double division {
+get { return this.add/sub;}
+}
+
+}
+
+
+}
