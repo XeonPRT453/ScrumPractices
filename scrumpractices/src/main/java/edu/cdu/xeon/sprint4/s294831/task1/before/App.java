f0d2dce3f235e4e518e9a498648d95c7ddc63d02
shraddha294831 <30825232+shraddha294831@users.noreply.github.com>
2019-05-15 05:05:29 +0930

    Update and rename App.java to Factory.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint4/s294831/task1/before/App.java
+++
@@ -1,4 +0,0 @@
-package edu.cdu.xeon.sprint4.s294831.task1.before;
-
-public class App {
-}
---
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint4/s294831/task1/before/Factory.java
@@ -0,0 +1,14 @@
+package edu.cdu.xeon.sprint4.s294831.task1.before;
+
+public class Factory {
+
+
+
+/**
+ * Common interface for all buttons.
+ */
+public interface Button {
+    void render();
+    void onClick();
+}
+}
