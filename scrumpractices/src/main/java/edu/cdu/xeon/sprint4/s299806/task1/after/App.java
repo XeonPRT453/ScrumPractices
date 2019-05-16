24683d09a2283fac44814179aae28bd61321b95d
JerryKang666 <30918020+JerryKang666@users.noreply.github.com>
2019-05-17 04:27:20 +0930

    Update App.java

--- scrumpractices/src/main/java/edu/cdu/xeon/sprint4/s299806/task1/after/App.java
+++ scrumpractices/src/main/java/edu/cdu/xeon/sprint4/s299806/task1/after/App.java
@@ -1,4 +1,28 @@
-package edu.cdu.xeon.sprint4.s299806.task1.after;
+using System.Collections.Generic;
+using System.Linq;
+using LosTechies.DaysOfRefactoring.SampleCode.BreakMethod.After;
+using Customer = LosTechies.DaysOfRefactoring.BreakResponsibilities.After.Customer;
 
-public class App {
+namespace LosTechies.DaysOfRefactoring.SampleCode.ReturnASAP.After
+{
+    public class Order
+    {
+        public Customer Customer { get; private set; }
+
+        public decimal CalculateOrder(Customer customer, IEnumerable<Product> products, decimal discounts)
+        {
+            if (products.Count() == 0)
+                return 0;
+
+            Customer = customer;
+            decimal orderTotal = products.Sum(p => p.Price);
+
+            if (discounts == 0)
+                return orderTotal;
+
+            orderTotal -= discounts;
+
+            return orderTotal;
+        }
+    }
 }
